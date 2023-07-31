c54a27d9583e631987d42adba3397c335f316789
Rômulo Bovi <58385939+romulobovi@users.noreply.github.com>
2023-07-31 13:10:23 -0300

    welcome4.java

---
+++ Welcome4.java
@@ -0,0 +1,11 @@
+// Exibindo múltiplas linhas com o método System.out.printf.
+
+public class Welcome4
+{
+    // método main incia a execução do aplicativo Java
+    public static void main (String[] args)
+    {
+        System.out.printf("%s%n%s%n",
+            "Welcome to", "Java Programming!");          
+    } // fim do método main
+} // fim da classe Welcome4
