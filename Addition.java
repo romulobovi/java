367b4a43d719271e7558e2e95bcc49507fdbe9ec
Rômulo Bovi <58385939+romulobovi@users.noreply.github.com>
2023-08-01 17:37:04 -0300

    Addition.java

---
+++ Addition.java
@@ -0,0 +1,11 @@
+// Programa de adição que insere dois números, então exibe a soma deles.
+import java.util.Scanner; // program utiliza a classe Scanner
+
+public class Addition
+{
+    // método main inicia a execução do aplicativo Java
+    public static void main (String[] args)
+    {
+        
+    }
+}
